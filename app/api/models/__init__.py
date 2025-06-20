6754e5f39cc055cf44c1a11bd95124e7de41a7a4
Miguel Ángel <146471207+mangelccc@users.noreply.github.com>
2025-06-20 12:54:59 +0200

    continuacion

--- app/api/models/__init__.py
+++ app/api/models/__init__.py
@@ -1,6 +1,3 @@
 # Exportar los modelos para facilitar su importación
 from app.api.models.Chat import ChatMessage, ChatResponse
-
-# Exportar los modelos para facilitar su importación
-from app.api.models.Chat import ChatMessage, ChatResponse
 from app.api.models.User import Usuario, UsuariosResponse, CreateUsuario, UpdateUsuario, DeleteUsuarioResponse
